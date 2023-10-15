536e85d161b7f62ed4f7e2788a5f39a4cfbda40a
hrushikesh <hrushikesh.kyathari@aidash.com>
2023-10-15 19:26:20 +0530

    removed all middleware

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routers import routers_main
 import os
-from fastapi import Request
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
 
-app.add_middleware(HTTPSRedirectMiddleware)
 
 os.makedirs("static/stacked", exist_ok=True)
 os.makedirs("static/perturbations", exist_ok=True)
@@ -16,14 +13,5 @@ os.makedirs("static/adversarial", exist_ok=True)
 
 #Mount the static files directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.middleware("http")
-async def middleware_events(request: Request, call_next):
-    if os.getenv("FASTAPI_ENV") != "local":
-        request.scope["scheme"] = "https"
-        
-    response = await call_next(request)
-
-    return response
 
 app.include_router(routers_main.router)
